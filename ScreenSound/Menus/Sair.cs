fa14c0f2507caf4e9eea1f10fd896d04f7458b09
Thiago Santos <thiago.santos.gomes.00@gmail.com>
2024-08-06 11:08:51 -0300

    3° Comportamentos comuns aos menus

---
+++ ScreenSound/Menus/Sair.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Moodelos;
+
+namespace ScreenSound.Menus;
+
+internal class Sair : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.WriteLine("Até mais :)");
+    }
+}
